15bd786ae9b971150808d82f38cc85db4f7b3e90
Vinicius Ottoni <rinottoni@gmail.com>
2023-04-26 20:42:37 -0300

    Resources OK + Início do Infra.Data

--- RecrutaPlus.Infra.Data/AppDataSettingsConfigutarionConst.cs
+++ RecrutaPlus.Infra.Data/AppDataSettingsConfigutarionConst.cs
@@ -2,6 +2,21 @@
 {
     public struct AppDataSettingsConfigutarionConst
     {
+        //Settings
+        public static string AppSettings => "appSettings";
 
+        //DbProviderFactory
+        public static string DBProviderFactoryType => "DBProviderFactoryType";
+        public static string DBProviderFactoryName => "DBProviderFactoryName";
+        public static string ConnectionStringDefault => "ConnectionStringDefault";
+
+        //DbProviderFactory Integration
+        public static string DbProviderFactoryTypeIntegration => "DbProviderFactoryTypeIntegration";
+        public static string DbProviderFactoryNameIntegration => "DbProviderFactoryNameIntegration";
+        public static string ConnectionStringDefaultIntegration => "ConnectionStringDefaultIntegration";
+
+        public static string DbProviderFactoryTypeMsSQL => "MsSQL";
+        public static string DbProviderFactoryTypeOracle => "Oracle";
+        public static string DbProviderFactoryTypeMySQL => "MySQL";
     }
 }
